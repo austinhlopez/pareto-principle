a58048ba47c33770fcbb88606d3b6ce3b209b4e5
Kyle Erf <kerf@rjmetrics.com>
2012-07-25 14:47:41 -0400

    workign backend kinda

--- ParetoAnalyzer.py
+++ ParetoAnalyzer.py
@@ -4,7 +4,9 @@ import sys
 class ParetoAnalyzer:
     def __init__(self, csvFileName):
 
-        self.rows = self.get_data_from_csv(csvFileName)
+        dataRows = self.get_data_from_csv(csvFileName)
+        self.dataBuckets = self.get_percentiles(dataRows)
+
 
     def get_data_from_csv(self, filename):
         reader = csv.reader(open(filename, 'rb'), delimiter=',' , quotechar = '"')
@@ -20,14 +22,33 @@ class ParetoAnalyzer:
                     idDict[curRow[0]] += curRow[1]
         
             sortedItems = sorted(idDict.items(), key = lambda x: x[1])
-
-            print sortedItems[::-1][:50]
+            return sortedItems            
 
         except IOError as e:
             print "BULLOCKS", e
 
 
+    def get_percentiles(self, sortedData):
+        numItems = len(sortedData)
 
+        cdf = [0]
+
+        curUserGroup = []
+        userGroups = [[]]
+
+        nextPercentile = 1
+        currentCDF = 0
+
+        for i in range(0, len(sortedData)):
+            currentCDF += sortedData[i][1]
+            curUserGroup.append(sortedData[i])
+            if((i+1)*100/len(sortedData) == nextPercentile):
+                cdf.append(currentCDF)
+                userGroups.append(curUserGroup)
+                curUserGroup = []
+                nextPercentile += 1
+        
+#percent of users, portion of users, percent of rev., portion of rev
 
 
 testP = ParetoAnalyzer(sys.argv[1])
