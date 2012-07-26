ebc56fa144aef34d4434f53992a1bf7b549a1ffc
Austin Lopez <austinhlopez@gmail.com>
2012-07-26 15:58:40 -0400

    First page of desktop app. Users can click on a csv and it will automatically populate the textbox. Need to pull Kyle's stuff

--- practiceApp.py
+++ practiceApp.py
@@ -13,9 +13,9 @@ class MyFrame(wx.Frame):
         #self.split1 = wx.SplitterWindow(self)
         #self.split2 = wx.SplitterWindow(self.split1)
         
-        self.leftPanel = leftPanel(self.split1)
+        self.leftPanel = leftPanel(self, self.split1)
         
-        self.rightPanel = rightPanel(self.split1)
+        self.rightPanel = rightPanel(self, self.split1)
         
         #Add controls to the splitters:
         self.split1.SplitVertically(self.leftPanel, self.rightPanel)
@@ -26,53 +26,64 @@ class MyFrame(wx.Frame):
         #      self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
         self.Show(True)
 
+    def populateTextCtrl(self, filePath):
+        self.rightPanel.populateTextCtrl(filePath);
+
     
 
 #Left Panel: Contains directory and handles traversal.
 class leftPanel(wx.Panel):
-    def __init__(self, parent, *args, **kwargs):
+    def __init__(self, primaryFrame, parent, *args, **kwargs):
         # Create the panel.
         wx.Panel.__init__(self, parent, *args, **kwargs)
 
         self.parent = parent
-        #self.dir = wx.GenericDirCtrl(self, -1, size=(900,600), style=0, filter = "*.csv")
+        self.primaryFrame = primaryFrame
+        self.dir = wx.GenericDirCtrl(self, -1, size=(900,600), style=0, filter = "*.csv")
 
-        #       self.Bind(wx.EVT_TREE_SEL_CHANGED, self.onSel, self.dir.TreeCtrl)
+        self.Bind(wx.EVT_TREE_SEL_CHANGED, self.onSel, self.dir.TreeCtrl)
     
-        def onSel(self):
-            pass
-        #     txt1 = wx.St
+    def onSel(self, event):
+        self.primaryFrame.populateTextCtrl(self.dir.GetFilePath())
 
 #Right Panel: CSV Upload bar and instructions.
 class rightPanel(wx.Panel):
-    def __init__(self, parent, *args, **kwargs):
+    def __init__(self, primaryFrame, parent, *args, **kwargs):
         # Create the panel.
         wx.Panel.__init__(self, parent, *args, **kwargs)
-        self.SetBackgroundColour('cyan')
-
 
         self.parent = parent
+        self.primaryFrame = primaryFrame
 
         #Right Panel: Instructions on formatting, upload file bar
         self.button = wx.Button(self, label="Calculate")
         self.fileUpload = wx.TextCtrl(self, style=wx.TE_PROCESS_ENTER)
-
+        
         # Add buttons, file uploader.
         self.uploadSizer = wx.BoxSizer(wx.VERTICAL)
         
         self.uploadBar = wx.BoxSizer(wx.HORIZONTAL)
-        self.uploadBar.Add(self.fileUpload, 1)
-        self.uploadBar.Add(self.button, 1)
+        self.uploadBar.Add(self.fileUpload, 1, wx.EXPAND)
+        self.uploadBar.Add(self.button, 0, wx.EXPAND)
 
-        self.instructions = wx.StaticText(self, label="""1. Upload a CSV file \n\n Your CSV should contain two columns: a column of item IDs and a column of transaction amounts, separated by commas. No headers please. \n\n Example: \n
- '1321', 24.95 \n '1322', 16.50 \n '1321', 22.95""")
+        self.SetAutoLayout(True)
+        
+        self.instructions = wx.StaticText(self, label="""1. Upload a CSV file \n\n Browse for your CSV on the left, or enter the full path name in the text box below. Your CSV should contain two columns: a column of item IDs and a column of transaction amounts, separated by commas. No headers please. \n\n Example: \n
+ '1321', 24.95 \n '1322', 16.50 \n '1321', 22.95 \n\n\n""")
 
         self.uploadSizer.Add(self.instructions)
-        self.uploadSizer.Add(self.uploadBar)
-
+        self.uploadSizer.Add(self.uploadBar, .2, wx.EXPAND)
+       
         self.SetSizer(self.uploadSizer)
         self.SetAutoLayout(True)
+        self.uploadBar.Fit(self)
         self.uploadSizer.Fit(self)
+        
+        self.instructions.Wrap(430)
+
+
+    def populateTextCtrl(self, filePath):
+        self.fileUpload.WriteText(filePath);
 
 class ProportionalSplitter(wx.SplitterWindow):
     def __init__(self,parent, id = -1, proportion=0.66, size = wx.DefaultSize, **kwargs):
